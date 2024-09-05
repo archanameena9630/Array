f0a8c88d8a9eb16391506950a0c4c5c0d8fa335b
archanameena <169147657+archanameena9630@users.noreply.github.com>
2024-09-05 23:14:41 +0530

    Add files via upload

---
+++ length_count.java
@@ -0,0 +1,10 @@
+package Arry;
+
+
+// Java Program to count total number of elements in array
+public class Arry12 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,10};
+        System.out.println(arr.length);
+    }
+}
